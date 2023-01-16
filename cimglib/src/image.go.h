dc1d2c5755e5fbecfb3ebaebceb109152515753e
aeoum <aleskoum@gmail.com>
2023-01-16 03:10:24 +0100

    Use C++ headers instead

--- cimglib/src/image.go.h
+++ cimglib/src/image.go.h
@@ -1,13 +1,13 @@
 #ifndef CIMGLIB_IMAGE_GO_H
 #define CIMGLIB_IMAGE_GO_H
 
-#include <stdint.h>
+#include <cstdint>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-const char *CIMGLIB_MD5FromBytes(uint8_t *data, size_t dataSize, double size);
+const char *CIMGLIB_MD5FromBytes(std::uint8_t *data, size_t dataSize, double size);
 const char *CIMGLIB_MD5FromFile(char *path, double size);
 
 #ifdef __cplusplus
