59424c5792a9ee61260bca2928c9b4b5a437497f
aeoum <aleskoum@gmail.com>
2023-01-19 10:21:37 +0100

    Type typo

--- cimglib/src/image.cpp
+++ cimglib/src/image.cpp
@@ -79,6 +79,6 @@ const std::string Image::Hash()
     return ss.str();
     */
     MD5 md5;
-    md5.add(pixelMean.data(), pixelMean.size() * sizeof(dobuel));
+    md5.add(pixelMean.data(), pixelMean.size() * sizeof(double));
     return md5.getHash();
 }
