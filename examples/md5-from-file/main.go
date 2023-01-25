b0296a47bbde77d7e27cf7ca9241c5b08fc3afe2
aeoum <aleskoum@gmail.com>
2023-01-25 11:50:45 +0100

    test

--- examples/md5-from-file/main.go
+++ examples/md5-from-file/main.go
@@ -8,6 +8,6 @@ import (
 
 func main() {
 	var size float64 = 224
-	hash := cgoimg.MD5FromFile("../../testdata/0120001c599dc0af44615e7fe8c41b255a0d288d", size)
+	hash := cgoimg.MD5FromFile("img", size)
 	fmt.Printf("Hash: %s\n", hash)
 }
